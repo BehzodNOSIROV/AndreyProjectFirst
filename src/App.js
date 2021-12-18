5d471de7af4fd40dcfc8f40c77713241abb1d584
behzod <behzodnosirovofficial@gmail.com>
2021-12-18 10:05:43 +0500

    yemadi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./App.css";
+// import { Route } from "react-router-dom"
 import HomePage from "./pages/homepage/homepage.componenet";
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
+
+// function App() {
+//   return (
+//     <div>
+//       <Route path="/" component={HomePage} />
+//       <Route path="/hats" component={HatsPage} />
+//     </div>
+//   );
+// }
 function App() {
   return (
     <div>
-      <HomePage />
+      <HomePage/>
+      <HatsPage/>
     </div>
   );
 }
